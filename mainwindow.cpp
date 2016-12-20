d3b9eb6e52b998a6f31989d91ace10d948f2470c
Kislenko Maksim <kmg.xammi.1@gmail.com>
2016-12-20 14:01:45 +0300

    Relaxation method

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,7 +21,7 @@ void get_end_time(Time start_time) {
     int seconds = (qFloor(sec) % 3600) % 60;
     int millis = qFloor(sec * 1000) % 1000;
 
-    QString timing = "Затраченное время:";
+    QString timing = "Затраченное время: ";
     if (hours > 0) timing += QString::number(hours) + " часов ";
     if (minutes > 0) timing += QString::number(minutes) + " минут ";
     if (seconds > 0) timing += QString::number(seconds) + "." + QString::number(millis) + " секунд";
@@ -47,9 +47,15 @@ MainWindow::~MainWindow() {
 void MainWindow::recalculate() {
     ui->T->setText("");
     ui->error->setText("");
+    QString method = ui->cmb_method->currentText();
     try {
         Doubles2D Ts;
-        this->straight_method(Ts);
+        if (method.startsWith("Прямой")) {
+            this->straight_method(Ts);
+        }
+        else if (method.startsWith("Установления")) {
+            this->relaxation_method(Ts);
+        }
         this->view_result(Ts);
     }
     catch (QString error) {
@@ -185,8 +191,6 @@ void MainWindow::relaxation_method(Doubles2D & Ts) throw (QString) {
     double Hx2 = Hx * Hx, Hz2 = Hz * Hz;
     int Nx_1 = Nx - 1, Nz_1 = Nz - 1, Nx_2 = Nx - 2, Nz_2 = Nz - 2;
 
-    Time start_time = get_current_time();
-
     Doubles2D lambdas;
     rs.init_lambdas(U0, Nx, Nz, lambdas);
     Doubles2D Ts_strich, Ts_roof, prev_Ts;
@@ -194,30 +198,37 @@ void MainWindow::relaxation_method(Doubles2D & Ts) throw (QString) {
 
     Doubles alphas_I, betas_I;
     alphas_I.fill(0.0, Nx); betas_I.fill(0.0, Nx);
-    auto A1 = [&lambdas, Hx2] (I, J) {
+    auto A1 = [&lambdas, Hx2] (int I, int J) {
         return (lambdas[I][J] + lambdas[I][J + 1]) / 2 / Hx2;
     };
-    auto C1 = [&lambdas, Hx2, tau] (I, J) {
-        return (-1) * (2 * lambdas[I][J] + lambdas[I][J + 1] + lambdas[I][J - 1]) / 2 / Hx2 + 1 / tau;
+    auto C1 = [&lambdas, Hx2, tau] (int I, int J) {
+        return (-1) * (2 * lambdas[I][J] + lambdas[I][J + 1] + lambdas[I][J - 1]) / 2 / Hx2 + (1 / tau);
     };
-    auto B1 = [&lambdas, Hx2] (I, J) {
+    auto B1 = [&lambdas, Hx2] (int I, int J) {
         return (lambdas[I][J] + lambdas[I][J - 1]) / 2 / Hx2;
     };
-    auto F1 = [this, &Ts, F0, tau, Hz] (I, J) {
-        return (-1) * this->rs.calc_f(F0, Hz * I, Ts[I][J]) / 2 + Ts[I][J] / tau;
+    auto F1 = [this, &Ts, F0, tau, Hz] (int I, int J) {
+        return (-1) * this->rs.calc_f(F0, Hz * I, Ts[I][J]) / 2 + (Ts[I][J] / tau);
     };
 
     Doubles alphas_J, betas_J;
     alphas_J.fill(0.0, Nz); betas_J.fill(0.0, Nz);
+    auto A2 = [&lambdas, Hz2] (int I, int J) {
+        return (lambdas[I][J] + lambdas[I + 1][J]) / 2 / Hz2;
+    };
+    auto C2 = [&lambdas, Hz2, tau] (int I, int J) {
+        return (-1) * (2 * lambdas[I][J] + lambdas[I + 1][J] + lambdas[I - 1][J]) / 2 / Hz2 + (1 / tau);
+    };
+    auto B2 = [&lambdas, Hz2] (int I, int J) {
+        return (lambdas[I][J] + lambdas[I - 1][J]) / 2 / Hz2;
+    };
+    auto F2 = [this, tau, &Ts_strich, F0, Hz] (int I, int J) {
+        return (-1) * this->rs.calc_f(F0, Hz * I, Ts_strich[I][J]) / 2 + (Ts_strich[I][J] / tau);
+    };
 
+    Time start_time = get_current_time();
     int counter = 0;
     while (rs.if_stop_iterations(Nx, Nz, Ts, prev_Ts)) {
-        prev_Ts = Ts;
-        if (counter > 0) {
-            Ts = Ts_roof;
-            rs.recalc_lambdas(Nx, Nz, Ts, lambdas);
-        }
-
         // прогонка для фиксированных I
         for (int I = 1; I < Nz_1; I++) {
             double C_I1 = C1(I, 1);
@@ -225,7 +236,7 @@ void MainWindow::relaxation_method(Doubles2D & Ts) throw (QString) {
             betas_I[2] = F1(I, 1) / C_I1;
 
             // вычисление прогоночных коэффициентов
-            for (int J = 1; J < Nx_1; J++) {
+            for (int J = 2; J < Nx_1; J++) {
                 double A_J = A1(I, J);
                 double znam = (A_J * alphas_I[J] + C1(I, J));
                 alphas_I[J + 1] = -B1(I, J) / znam;
@@ -236,11 +247,11 @@ void MainWindow::relaxation_method(Doubles2D & Ts) throw (QString) {
 
             // по краевому условию (4)
             double K4 = alpha * Hz / lambdas[I][Nx_1];
-            Ts_strich[I][Nx_1] = Ts_strich[I][Nx_2] + K4 * U0 / (1 + K4);
+            Ts_strich[I][Nx_1] = (Ts_strich[I][Nx_2] + K4 * U0) / (1 + K4);
 
             // восстановление Y с надчёркиванием
             for (int J = Nx_2 - 1; J > 0; J--) {
-                Ts_strich[I][J] = alphas_I[J] * Ts_strich[I][J + 1] + betas_I[J];
+                Ts_strich[I][J] = alphas_I[J + 1] * Ts_strich[I][J + 1] + betas_I[J + 1];
             }
 
             // по краевому условию (3)
@@ -253,10 +264,50 @@ void MainWindow::relaxation_method(Doubles2D & Ts) throw (QString) {
 
             // по краевому условию (5)
             double K5 = alpha * Hz / lambdas[Nz_1][J];
-            Ts_strich[Nz_1][J] = Ts_strich[Nz_2][J] * (1 + K5) - K5 * U0;
+            Ts_strich[Nz_1][J] = (Ts_strich[Nz_2][J] - K5 * U0) / (1 + K5);
         }
 
-        // resolve to Ts_roof basing on Ts_strich
+        // прогонка для фиксированных J
+        for (int J = 1; J < Nx_1; J++) {
+            double C_1J = C2(1, J);
+            alphas_J[2] = -B2(1, J) / C_1J;
+            betas_J[2] = F2(1, J) / C_1J;
+
+            // вычисление прогоночных коэффициентов
+            for (int I = 2; I < Nz_1; I++) {
+                double A_I = A2(I, J);
+                double znam = (A_I * alphas_J[I] + C2(I, J));
+                alphas_J[I + 1] = -B2(I, J) / znam;
+                betas_J[I + 1] = (F2(I, J) - A_I * betas_J[I]) / znam;
+            }
+            double A_Nz = A2(Nz_2, J);
+            Ts_roof[Nz_2][J] = (F2(Nz_2, J) - A_Nz * betas_J[Nz_2]) / (C2(Nz_2, J) + A_Nz * alphas_J[Nz_2]);
+
+            // по краевому условию (5)
+            double K5 = alpha * Hz / lambdas[Nz_1][J];
+            Ts_roof[Nz_1][J] = (Ts_roof[Nz_2][J] + K5 * U0) / (1 + K5);
+
+            // восстановление Y с крышкой
+            for (int I = Nz_2 - 1; I > 0; I--) {
+                Ts_roof[I][J] = alphas_J[I + 1] * Ts_roof[I + 1][J] + betas_J[I + 1];
+            }
+
+            // по краевому условию (2)
+            Ts_roof[0][J] = Ts_roof[1][J] + Hz * Ft / lambdas[0][J];
+        }
+        for (int I = 0; I < Nz; I++) {
+            // по краевому условию (3)
+            double K3 = alpha * Hx / lambdas[I][0];
+            Ts_roof[I][0] = (Ts_roof[I][1] - K3 * U0) / (1 + K3);
+
+            // по краевому условию (4)
+            double K4 = alpha * Hx / lambdas[I][Nx_1];
+            Ts_roof[I][Nx_1] = (Ts_roof[I][Nx_2] + K4 * U0) / (1 + K4);
+        }
+
+        prev_Ts = Ts;
+        Ts = Ts_roof;
+        rs.recalc_lambdas(Nx, Nz, Ts, lambdas);
 
         counter++;
         qDebug() << "Итерация №:" << counter;
